c194b3239df077cd3af6448cb2337d3dea8c04c2
Daniel Gimbatschki <Daniel.Gimbatschki@student.hpi.uni-potsdam.de>
2013-11-07 19:18:11 +0100

    added missing dependencies & added qtcreator files to gitignore

--- tools/autosetup/install.sh
+++ tools/autosetup/install.sh
@@ -2,6 +2,6 @@
 
 BASEDIR=$(dirname $0)
 
-apt-get install build-essential binutils-doc autoconf libboost-all-dev curl bzip2 ccache cmake liblog4cxx10-dev libnuma-dev libev-dev libhwloc-dev libbfd-dev
+apt-get install mysql-client-core-5.5 build-essential binutils-doc autoconf libboost-all-dev curl bzip2 ccache cmake liblog4cxx10-dev libnuma-dev libev-dev libhwloc-dev libbfd-dev libev-dev libtbb-dev python-requests
 
 sh -ex $BASEDIR/hyriseExternalDeps.sh
