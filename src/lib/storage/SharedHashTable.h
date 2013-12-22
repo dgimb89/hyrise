10d0fe2e916dd6aa9173789ae39402a6c6a1598f
Daniel Gimbatschki <Daniel.Gimbatschki@student.hpi.uni-potsdam.de>
2013-12-22 10:05:17 +0100

    Shared HT interface

--- src/lib/storage/SharedHashTable.h
+++ src/lib/storage/SharedHashTable.h
@@ -4,4 +4,23 @@
 #include "storage/HashTable.h"
 #include <mutex>
 
+template <class MAP, class KEY>
+class SharedHashTable : public HashTableBase<std::shared_ptr<MAP>,KEY>, public std::enable_shared_from_this<SharedHashTable<MAP,KEY>> {
+public:
+    typedef HashTableBase<std::shared_ptr<MAP>,KEY> base_t;
+    typedef KEY key_t;
+    typedef std::shared_ptr<MAP> map_ptr_t;
+
+    SharedHashTable(hyrise::storage::c_atable_ptr_t t, const field_list_t &f, size_t row_offset = 0)
+        : base_t(t, f) {
+        // create new map container
+    }
+
+    SharedHashTable(hyrise::storage::c_atable_ptr_t t, const field_list_t &f, map_ptr_t map, size_t row_offset = 0)
+        : base_t(t, f), base_t::_map(map) {
+
+    }
+
+};
+
 #endif // SRC_LIB_STORAGE_SHAREDHASHTABLE_H
